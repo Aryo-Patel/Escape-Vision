8b7e581908409df81ee70fc17196aa4df463bda7
AryoP <aryopatel@gmail.com>
2020-04-11 19:29:36 -0500

    cleaned up the builder file; gave option to upload image with question

--- client/src/components/builder/Builder.js
+++ client/src/components/builder/Builder.js
@@ -1,37 +1,60 @@
-import React, {Fragment} from 'react';
-import {Link} from 'react-router-dom'
+import React, {Fragment, useState} from 'react';
+import {Redirect, Link } from 'react-router-dom'
 import '../../CSS/builder/builder.css';
 /*This is the component that a teacher will be redirected to after they select
 what subject they are going to be teaching. I am going to add some redux to the
 app so that the application can keep track of what subject the teacher is planning
 on teaching, but that will come later */
 
-
-//Currently working on allowing a user to upload a photo as a question
+//For development purposes the number of questions is set to 1
 const Builder = () => {
     //This code sets the number of questions that the teacher will add.
     //This will become a scalable value later on
-    const NUMBER_OF_QUESTIONS = 10;
+    const NUMBER_OF_QUESTIONS = 1;
     const questionArray = new Array(NUMBER_OF_QUESTIONS);
     for(let i = 1; i <= NUMBER_OF_QUESTIONS; i++){
         questionArray[i] = i;
     }
 
+    //State component checks to see if form has been submitted, if it has,
+    //state tells the redirect component to redirect to choosing a game screen
+    const [redirect, toggleRedirect] = useState(false);
+
+    //on submit will redirect to chosing the game if all fields are filled out
+    const onSubmit = e => {
+        e.preventDefault();
+
+        toggleRedirect(true);
+    }
+
+    //fucntion to check state of redirect
+    const checkRedirect = () =>{
+        if(redirect){
+            return <Redirect to = '/game-chooser' />
+        }
+    }
+
+
     return (
         <Fragment>
+        {checkRedirect()}
         <div className = "builder-top-spacer"></div>
         {/*Link allows user to go back to the subject selector page */}
         <Link className = "builder-back-button" to = "/">Back</Link>
-        <form className = 'builder-container'>
-            <h3>Enter the problems along with the correct answers here</h3>
+        <form className = 'builder-container' onSubmit = {e => onSubmit(e)}>
+            <h3>Enter the problems along with the correct answers here <br/>
+            Text is required, but uploading a file is optional</h3>
             <div className = "question-answer-container">
                 <div className = "questions-container">
                     <span>Questions</span>
                     <ul>
     {/*mapping through the number of questions and creating an input for that */}
                         {questionArray.map(question =>(
-                            <li id = {question} className = "question-li">
-                                <input placeholder = {question} />
+                            <li id = {NUMBER_OF_QUESTIONS + question} className = "question-li">
+                                <div className = 'question-image-text-holder'>
+                                    <input type = 'file' accept = "image/*" />
+                                    <input placeholder = {question}  required/>
+                                </div>
                             </li>
                         ))}
                     </ul>
@@ -40,15 +63,17 @@ const Builder = () => {
                     <span>Answers</span>
                     <ul>
     {/*mapping through the number of questions and creating an input for that */}
-                        {questionArray.map(question =>(
-                            <li id = {question} className = "question-li">
-                                <input placeholder = {question} />
+                        {questionArray.map(answer =>(
+                            <li id = {answer} className = "question-li">
+                                <input placeholder = {answer} required/>
                             </li>
                         ))}
                     </ul>
                 </div>
             </div>
-            <Link className = "builder-submit" to = '/game-chooser'>Submit</Link>
+            <div className= 'builder-input-holder'>
+                <input type = 'submit' />
+            </div>
         </form>
         </Fragment>
         
